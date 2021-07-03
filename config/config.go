6eb58da77974041ddb95dd400fe5ecdc161ac069
Marcin Budny <marcinbudny@users.noreply.github.com>
2021-07-03 20:08:51 +0200

    Refactoring (#18)
    * refactored to packages
    * initial work on tests
    * initial work on CI

---
+++ config/config.go
@@ -0,0 +1,72 @@
+package config
+
+import (
+	"errors"
+	"fmt"
+	"time"
+
+	"github.com/namsral/flag"
+)
+
+type Config struct {
+	Timeout            time.Duration
+	Port               uint
+	Verbose            bool
+	InsecureSkipVerify bool
+
+	EventStoreURL             string
+	EventStoreUser            string
+	EventStorePassword        string
+	ClusterMode               string
+	EnableParkedMessagesStats bool
+}
+
+func createFlagSet(config *Config) *flag.FlagSet {
+	fs := flag.NewFlagSet("flagset", flag.ContinueOnError)
+	fs.StringVar(&config.EventStoreURL, "eventstore-url", "http://localhost:2113", "EventStore URL")
+	fs.StringVar(&config.EventStoreUser, "eventstore-user", "", "EventStore User")
+	fs.StringVar(&config.EventStorePassword, "eventstore-password", "", "EventStore Password")
+	fs.UintVar(&config.Port, "port", 9448, "Port to expose scraping endpoint on")
+	fs.DurationVar(&config.Timeout, "timeout", time.Second*10, "Timeout when calling EventStore")
+	fs.BoolVar(&config.Verbose, "verbose", false, "Enable verbose logging")
+	fs.StringVar(&config.ClusterMode, "cluster-mode", "cluster", "Cluster mode: `cluster` or `single`. In single mode, calls to cluster status endpoints are skipped")
+	fs.BoolVar(&config.InsecureSkipVerify, "insecure-skip-verify", false, "Skip TLS certificatte verification for EventStore HTTP client")
+	fs.BoolVar(&config.EnableParkedMessagesStats, "enable-parked-messages-stats", false, "Enable parked messages stats scraping")
+
+	return fs
+}
+
+func Load(args []string, suppressOutput bool) (*Config, error) {
+	config := &Config{}
+	fs := createFlagSet(config)
+
+	if suppressOutput {
+		fs.Usage = func() {}
+	}
+
+	if err := fs.Parse(args); err == nil {
+		if validationErr := config.validate(); validationErr == nil {
+			return config, nil
+		} else {
+			return nil, validationErr
+		}
+	} else {
+		return nil, err
+	}
+}
+
+func (config *Config) validate() error {
+	if config.ClusterMode != "cluster" && config.ClusterMode != "single" {
+		return fmt.Errorf("unknown cluster mode %v, use 'cluster' or 'single'", config.ClusterMode)
+	}
+
+	if (config.EventStoreUser != "" && config.EventStorePassword == "") || (config.EventStoreUser == "" && config.EventStorePassword != "") {
+		return errors.New("EventStore user and password should both be specified, or should both be empty")
+	}
+
+	return nil
+}
+
+func (config *Config) IsInClusterMode() bool {
+	return config.ClusterMode == "cluster"
+}
--- eventstore_exporter.go
+++ eventstore_exporter.go
@@ -1,107 +1,55 @@
 package main
 
 import (
-	"fmt"
-	"net/http"
-	"time"
+	"os"
 
-	"github.com/namsral/flag"
+	log "github.com/sirupsen/logrus"
 
-	"github.com/sirupsen/logrus"
-
-	"github.com/prometheus/client_golang/prometheus"
-	"github.com/prometheus/client_golang/prometheus/promhttp"
+	"github.com/marcinbudny/eventstore_exporter/client"
+	"github.com/marcinbudny/eventstore_exporter/collector"
+	"github.com/marcinbudny/eventstore_exporter/config"
+	"github.com/marcinbudny/eventstore_exporter/server"
 )
 
-var (
-	log = logrus.New()
+func readAndValidateConfig() *config.Config {
+	if config, err := config.Load(os.Args[1:], false); err == nil {
+		password := config.EventStorePassword
+		if password != "" {
+			password = "**REDACTED**"
+		}
+		log.WithFields(log.Fields{
+			"eventStoreURL":             config.EventStoreURL,
+			"eventStoreUser":            config.EventStoreUser,
+			"eventStorePassword":        password,
+			"port":                      config.Port,
+			"timeout":                   config.Timeout,
+			"verbose":                   config.Verbose,
+			"clusterMode":               config.ClusterMode,
+			"insecureSkipVerify":        config.InsecureSkipVerify,
+			"enableParkedMessagesStats": config.EnableParkedMessagesStats,
+		}).Infof("EventStore exporter configured")
 
-	timeout            time.Duration
-	port               uint
-	verbose            bool
-	insecureSkipVerify bool
-
-	eventStoreURL             string
-	eventStoreUser            string
-	eventStorePassword        string
-	clusterMode               string
-	enableParkedMessagesStats bool
-)
-
-func serveLandingPage() {
-	var landingPage = []byte(`<html>
-		<head><title>EventStore exporter for Prometheus</title></head>
-		<body>
-		<h1>EventStore exporter for Prometheus</h1>
-		<p><a href='/metrics'>Metrics</a></p>
-		</body>
-		</html>
-		`)
-
-	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		w.Write(landingPage) // nolint: errcheck
-	})
-}
-
-func serveMetrics() {
-	prometheus.MustRegister(NewCollector())
-
-	http.Handle("/metrics", promhttp.Handler())
-}
-
-func readAndValidateConfig() {
-	flag.StringVar(&eventStoreURL, "eventstore-url", "http://localhost:2113", "EventStore URL")
-	flag.StringVar(&eventStoreUser, "eventstore-user", "", "EventStore User")
-	flag.StringVar(&eventStorePassword, "eventstore-password", "", "EventStore Password")
-	flag.UintVar(&port, "port", 9448, "Port to expose scraping endpoint on")
-	flag.DurationVar(&timeout, "timeout", time.Second*10, "Timeout when calling EventStore")
-	flag.BoolVar(&verbose, "verbose", false, "Enable verbose logging")
-	flag.StringVar(&clusterMode, "cluster-mode", "cluster", "Cluster mode: `cluster` or `single`. In single mode, calls to cluster status endpoints are skipped")
-	flag.BoolVar(&insecureSkipVerify, "insecure-skip-verify", false, "Skip TLS certificatte verification for EventStore HTTP client")
-	flag.BoolVar(&enableParkedMessagesStats, "enable-parked-messages-stats", false, "Enable parked messages stats scraping")
-
-	flag.Parse()
-
-	if clusterMode != "cluster" && clusterMode != "single" {
-		log.Fatalf("Unknown cluster mode %v, use 'cluster' or 'single'", clusterMode)
-	}
-
-	if (eventStoreUser != "" && eventStorePassword == "") || (eventStoreUser == "" && eventStorePassword != "") {
-		log.Fatal("EventStore user and password should both be specified, or should both be empty")
-	}
-
-	log.WithFields(logrus.Fields{
-		"eventStoreURL":             eventStoreURL,
-		"eventStoreUser":            eventStoreUser,
-		"port":                      port,
-		"timeout":                   timeout,
-		"verbose":                   verbose,
-		"clusterMode":               clusterMode,
-		"insecureSkipVerify":        insecureSkipVerify,
-		"enableParkedMessagesStats": enableParkedMessagesStats,
-	}).Infof("EventStore exporter configured")
-}
-
-func setupLogger() {
-	if verbose {
-		log.Level = logrus.DebugLevel
+		return config
+	} else {
+		log.Fatal(err)
+		return nil
 	}
 }
 
-func startHTTPServer() {
-	listenAddr := fmt.Sprintf(":%d", port)
-	log.Fatal(http.ListenAndServe(listenAddr, nil))
+func setupLogger(config *config.Config) {
+	if config.Verbose {
+		log.SetLevel(log.DebugLevel)
+	}
 }
 
 func main() {
 
-	readAndValidateConfig()
-	setupLogger()
+	config := readAndValidateConfig()
+	setupLogger(config)
 
-	initializeClient()
+	client := client.New(config)
+	collector := collector.NewCollector(config, client)
 
-	serveLandingPage()
-	serveMetrics()
-
-	startHTTPServer()
+	exporterServer := server.NewExporterServer(config, collector)
+	exporterServer.ListenAndServe()
 }
