85cfa18b0ce62086b0891954683fbb650dab10a0
Marcin Budny <marcin.budny@gmail.com>
2021-08-21 18:34:47 +0200

    one more test script

---
+++ test_env/run-latest-docker-image.sh
@@ -0,0 +1,8 @@
+docker pull marcinbudny/eventstore_exporter:latest
+
+docker run -it --rm -p 9448:9448 \
+    -e EVENTSTORE_URL=http://host.docker.internal:2113 \
+    -e CLUSTER_MODE=single \
+    -e EVENTSTORE_USER=admin \
+    -e EVENTSTORE_PASSWORD=changeit \
+    marcinbudny/eventstore_exporter
