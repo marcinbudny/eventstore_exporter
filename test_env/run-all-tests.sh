cb5f6cd55796076d3d2f63654a2c728c157e2492
Marcin Budny <marcin.budny@gmail.com>
2022-01-22 22:05:59 +0100

    remove atompub leftovers

--- test_env/run-all-tests.sh
+++ test_env/run-all-tests.sh
@@ -18,12 +18,6 @@ sleep 5
 TEST_EVENTSTORE_URL=http://localhost:2113 TEST_PROJECTION_METRICS=1 go test ../... -count=1 -v
 docker-compose -f 20.10-single/docker-compose.yml down
 
-echo "Running tests on ES 20.10 - SINGLE - WITH ATOMPUB"
-EVENTSTORE_ENABLE_ATOM_PUB_OVER_HTTP=True docker-compose -f 20.10-single/docker-compose.yml up -d
-sleep 5
-TEST_EVENTSTORE_URL=http://localhost:2113 go test ../... -count=1 -v
-docker-compose -f 20.10-single/docker-compose.yml down
-
 echo "Running tests on ES 20.10 - CLUSTER"
 docker-compose -f 20.10-cluster/docker-compose.yml up -d
 sleep 15
@@ -40,12 +34,6 @@ echo "Running tests on ES 21.10 - SINGLE - WITH PROJECTIONS"
 EVENTSTORE_RUN_PROJECTIONS=All docker-compose -f 21.10-single/docker-compose.yml up -d
 sleep 5
 TEST_EVENTSTORE_URL=http://localhost:2113 TEST_PROJECTION_METRICS=1 go test ../... -count=1 -v
-docker-compose -f 21.10-single/docker-compose.yml down
-
-echo "Running tests on ES 21.10 - SINGLE - WITH ATOMPUB"
-EVENTSTORE_ENABLE_ATOM_PUB_OVER_HTTP=True docker-compose -f 21.10-single/docker-compose.yml up -d
-sleep 5
-TEST_EVENTSTORE_URL=http://localhost:2113 go test ../... -count=1 -v
 docker-compose -f 21.10-single/docker-compose.yml down
 
 echo "Running tests on ES 21.10 - CLUSTER"
