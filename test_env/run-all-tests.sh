480a2546f539b5e3047d0c9a3de8d68f4d05562f
Marcin Budny <marcin.budny@gmail.com>
2022-11-20 15:05:59 +0100

    22.10 test env

--- test_env/run-all-tests.sh
+++ test_env/run-all-tests.sh
@@ -5,7 +5,8 @@ docker-compose -f 20.10-single/docker-compose.yml down
 docker-compose -f 20.10-cluster/docker-compose.yml down
 docker-compose -f 21.10-single/docker-compose.yml down
 docker-compose -f 21.10-cluster/docker-compose.yml down
-docker-compose -f 22.6-single/docker-compose.yml down
+docker-compose -f 22.10-single/docker-compose.yml down
+docker-compose -f 22.10-cluster/docker-compose.yml down
 
 echo "Running tests on ES 20.10 - SINGLE"
 EVENTSTORE_RUN_PROJECTIONS=None docker-compose -f 20.10-single/docker-compose.yml up -d 
@@ -43,14 +44,20 @@ sleep 15
 TEST_EVENTSTORE_URL=https://localhost:2113 TEST_CLUSTER_MODE=cluster go test ../... -count=1 -v
 docker-compose -f 21.10-cluster/docker-compose.yml down
 
-echo "Running tests on ES 22.6 - SINGLE"
-EVENTSTORE_RUN_PROJECTIONS=None docker-compose -f 22.6-single/docker-compose.yml up -d
+echo "Running tests on ES 22.10 - SINGLE"
+EVENTSTORE_RUN_PROJECTIONS=None docker-compose -f 22.10-single/docker-compose.yml up -d
 sleep 5
 TEST_EVENTSTORE_URL=http://localhost:2113 go test ../... -count=1 -v
-docker-compose -f 22.6-single/docker-compose.yml down
+docker-compose -f 22.10-single/docker-compose.yml down
 
-echo "Running tests on ES 22.6 - SINGLE - WITH PROJECTIONS"
-EVENTSTORE_RUN_PROJECTIONS=All docker-compose -f 22.6-single/docker-compose.yml up -d
+echo "Running tests on ES 22.10 - SINGLE - WITH PROJECTIONS"
+EVENTSTORE_RUN_PROJECTIONS=All docker-compose -f 22.10-single/docker-compose.yml up -d
 sleep 5
 TEST_EVENTSTORE_URL=http://localhost:2113 TEST_PROJECTION_METRICS=1 go test ../... -count=1 -v
-docker-compose -f 22.6-single/docker-compose.yml down
+docker-compose -f 22.10-single/docker-compose.yml down
+
+echo "Running tests on ES 22.10 - CLUSTER"
+docker-compose -f 22.10-cluster/docker-compose.yml up -d
+sleep 15
+TEST_EVENTSTORE_URL=https://localhost:2113 TEST_CLUSTER_MODE=cluster go test ../... -count=1 -v
+docker-compose -f 22.10-cluster/docker-compose.yml down
