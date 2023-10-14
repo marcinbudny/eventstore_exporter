5be19299914da7d54b89c26c008a4a8208d9687c
Marcin Budny <marcin.budny@gmail.com>
2023-10-14 23:08:21 +0200

    added 23.6 test env

--- test_env/run-all-tests.sh
+++ test_env/run-all-tests.sh
@@ -5,6 +5,8 @@ docker-compose -f 21.10-single/docker-compose.yml down
 docker-compose -f 21.10-cluster/docker-compose.yml down
 docker-compose -f 22.10-single/docker-compose.yml down
 docker-compose -f 22.10-cluster/docker-compose.yml down
+docker-compose -f 23.06-single/docker-compose.yml down
+docker-compose -f 23.06-cluster/docker-compose.yml down
 
 echo "Running tests on ES 21.10 - SINGLE"
 EVENTSTORE_RUN_PROJECTIONS=None docker-compose -f 21.10-single/docker-compose.yml up -d 
@@ -41,3 +43,21 @@ docker-compose -f 22.10-cluster/docker-compose.yml up -d
 sleep 15
 go test ../... -count=1 -v
 docker-compose -f 22.10-cluster/docker-compose.yml down
+
+echo "Running tests on ES 23.06 - SINGLE"
+EVENTSTORE_RUN_PROJECTIONS=None docker-compose -f 23.06-single/docker-compose.yml up -d
+sleep 5
+go test ../... -count=1 -v
+docker-compose -f 23.06-single/docker-compose.yml down
+
+echo "Running tests on ES 23.06 - SINGLE - WITH PROJECTIONS"
+EVENTSTORE_RUN_PROJECTIONS=All docker-compose -f 23.06-single/docker-compose.yml up -d
+sleep 5
+go test ../... -count=1 -v
+docker-compose -f 23.06-single/docker-compose.yml down
+
+echo "Running tests on ES 23.06 - CLUSTER"
+docker-compose -f 23.06-cluster/docker-compose.yml up -d
+sleep 15
+go test ../... -count=1 -v
+docker-compose -f 23.06-cluster/docker-compose.yml down
